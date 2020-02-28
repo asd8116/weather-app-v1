c106ca09f8a34fbf81608b8fac4f5e1553f89f46
wanaka <asd8116@gmail.com>
2020-02-28 22:12:01 +0800

    Add gh-pages & Make PWA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import WeatherApp from './WeatherApp'
+import * as serviceWorker from './serviceWorker'
 
 // 這支 CSS 檔的樣式會作用到全域
 import './index.css'
@@ -12,3 +13,10 @@ function App() {
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
+// 將 unregister 改成 register
+serviceWorker.register()
